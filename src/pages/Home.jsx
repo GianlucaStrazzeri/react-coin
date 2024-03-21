b7f9616088b958a525f5e1b8af0557ae2706d09e
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-03-21 09:00:42 +0100

    tryng structure Home module

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,3 +13,25 @@ function Home() {
 }
 
 export default Home;
+
+//En Home busco imprimir un listado de crypto haré un fetch
+// fetch('api.coincap.io/v2/assets')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Hubo un problema con la solicitud: ' + response.status);
+//     }
+//     // Si la solicitud fue exitosa, devolver el cuerpo de la respuesta en formato JSON
+//     return response.json();
+//   })
+//   .then(data => { // Trabajar con los datos obtenidos   
+//let coins=data.map((coin)=>{
+//<div className="coinBox"> <h2>Nombre: {data.name}</h2>
+//imagen: {data.symbol}
+// precio: {data.preiceUsd} </div>
+//}) 
+//     console.log(coins);
+//   })
+//   .catch(error => {
+//     // Capturar cualquier error ocurrido durante el proceso
+//     console.error('Error al realizar la solicitud:', error);
+//   });
